0d8152fe6a3741238d40e8a70f42e260e75628d9
Rhys Morgan <rhysm@me.com>
2024-12-08 16:36:57 +0000

    Adds a solution for Day 3 Part 2 Those damn newlines

--- Tests/Day3Tests/Day3Tests.swift
+++ Tests/Day3Tests/Day3Tests.swift
@@ -10,7 +10,28 @@ import Testing
 
 let testInput = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
+let testInput2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
+let testInput3 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))mul(2,3)"
+
+let testInput4 = "xmul(1,3)don't()don't()mul(1,4)don't()do()mul(2,3)"
+
 @Test func part1_sample() throws {
   let part1 = try part1(testInput)
   #expect(part1 == 161)
+}
+
+@Test func part2_sample() throws {
+  let part2 = try part2(testInput2)
+  #expect(part2 == 48)
+}
+
+@Test func part2_sample2() throws {
+  let part2 = try part2(testInput3)
+  #expect(part2 == 54)
+}
+
+@Test func part2_sample3() throws {
+  let part2 = try part2(testInput4)
+  #expect(part2 == 9)
 }
